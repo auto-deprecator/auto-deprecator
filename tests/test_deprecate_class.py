aecedf2c5e55c2dd3be1157d0447235a71a8fafc
Gavin Chan <gavincyi@gmail.com>
2019-12-30 16:27:57 +0800

    Add depcreate class method unit tests

---
+++ tests/test_deprecate_class.py
@@ -0,0 +1,33 @@
+import pytest
+
+from tests.test_project.dummy_class import DummyClass
+
+
+def test_deprecate_version_2_0_0():
+    with pytest.raises(RuntimeError) as err:
+        obj = DummyClass()
+        obj.deprecate_version_2_0_0()
+
+    assert (
+        'Function "deprecate_version_2_0_0" is '
+        'deprecated since version 2.0.0') in str(err)
+
+
+def test_deprecate_version_2_1_0():
+    with pytest.raises(RuntimeError) as err:
+        obj = DummyClass()
+        obj.deprecate_version_2_1_0()
+
+    assert (
+        'Function "deprecate_version_2_1_0" is '
+        'deprecated since version 2.1.0') in str(err)
+
+
+def test_deprecate_version_2_2_0():
+    with pytest.warns(DeprecationWarning) as warning:
+        obj = DummyClass()
+        obj.deprecate_version_2_2_0()
+
+    assert (
+        'Function "deprecate_version_2_2_0" will '
+        'be deprecated on version 2.2.0') in warning[0].message.args[0]
