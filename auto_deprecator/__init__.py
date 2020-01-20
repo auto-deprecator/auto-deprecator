3c0daea3cbcdb5ac8f2ea6074119e214640f4d46
Gavin Chan <gavincyi@gmail.com>
2020-01-21 00:12:27 +0800

    Bump version 2020.1.1

--- auto_deprecator/__init__.py
+++ auto_deprecator/__init__.py
@@ -3,7 +3,7 @@
 
 __author__ = """Gavin Chan"""
 __email__ = "gavincyi@gmail.com"
-__version__ = "2020.1.0"
+__version__ = "2020.1.1"
 
 from auto_deprecator.deprecate import (
     deprecate,
--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
     tests_require=test_requirements,
     extras_require={"test": test_requirements,},
     url="https://github.com/gavincyi/auto_deprecator",
-    version="0.1.0",
+    version="2020.1.1",
     zip_safe=False,
     entry_points="""
         [console_scripts]
